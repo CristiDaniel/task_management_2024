0c6e152669c37a35cad55994b65e5b5afbac68f2
Cristi Daniel <danielcristi735@yahoo.com>
2024-12-09 23:26:36 +0200

    refactor: Removed ListOfTasks.tsx and ListOfTasks.module.css and added index.tsx and index.module.css instead

---
+++ frontend/src/features/tasks/index.tsx
@@ -0,0 +1,27 @@
+import styles from './index.module.css'
+import useListOfTasks from './hooks/useListOfTasks';
+import TaskItem from './components/TaskItem';
+import AddTaskForm from './components/AddTaskForm';
+
+export default function ListOfTasks() {
+  const {tasks, isLoading, error} = useListOfTasks();
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (error) return <div style={{color: 'red'}}>Error loading tasks</div>;
+
+  return (
+    <div className={styles.list_of_tasks_container}>
+      <AddTaskForm />
+      {
+        tasks.length !== 0 ?
+      <div className={styles.list_of_tasks}>
+        {tasks.map(task => <TaskItem {...task}/>)}
+      </div>
+      :
+      <p>There are no tasks</p>
+      }
+
+    </div>
+  )
+}
