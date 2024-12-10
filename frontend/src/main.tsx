10bbf56bee653e9810789019f37fd6731792f330
Cristi Daniel <danielcristi735@yahoo.com>
2024-12-10 20:52:29 +0200

    task(frontend): moved notification to the bottom and changed transition to Zoom

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,41 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import MainLayout from './Layouts/MainLayout'
-import Homepage from './Pages/Homepage'
-import './index.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
-
-
-
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./Layouts/MainLayout";
+import Homepage from "./Pages/Homepage";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ToastContainer, Zoom } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const queryClient = new QueryClient();
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <MainLayout />,
     children: [
       {
-        path: '/',
-        element: <Homepage />
+        path: "/",
+        element: <Homepage />,
       },
       {
-        path: '*',
-        element: 'Ruta nu a fost gasita'
-      }
-    ]
-  }
-])
+        path: "*",
+        element: "Ruta nu a fost gasita",
+      },
+    ],
+  },
+]);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ToastContainer theme='dark' />
-      <RouterProvider router={router}/>
+      <ToastContainer theme="dark" position="bottom-left" autoClose={2000} transition={Zoom} />
+      <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
