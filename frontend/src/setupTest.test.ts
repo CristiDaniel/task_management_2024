4867f18002c79fb6606f53bccae806cba6423ebc
Cristi Daniel <danielcristi735@yahoo.com>
2024-12-13 22:40:52 +0200

    task(frontend): added vitest setup

---
+++ frontend/src/setupTest.test.ts
@@ -0,0 +1,6 @@
+describe('It should run a test', () => {
+    it('simple addition test', () => {
+        const sum = 1 + 2;
+        expect(sum).toBe(3);
+      });
+})
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig, InlineConfig, UserConfig } from 'vite';
+
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true, 
+    environment: 'jsdom', 
+  },
+} as VitestConfigExport);
