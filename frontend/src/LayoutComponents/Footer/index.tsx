3be684111c6562fe7f46ebb627a9e71d8ec13a39
Cristi Daniel <danielcristi735@yahoo.com>
2024-12-14 22:40:04 +0200

    task(frontend): created a footer component and styled it

---
+++ frontend/src/LayoutComponents/Footer/index.tsx
@@ -0,0 +1,8 @@
+import styles from './index.module.css'
+const Footer = () => {
+    return <footer className={styles.footer}>
+        <span className={styles.copywright}>Task Management Project</span>
+    </footer>
+}
+
+export default Footer;
--- frontend/src/Layouts/MainLayout.tsx
+++ frontend/src/Layouts/MainLayout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../LayoutComponents/Header';
+import Footer from '../LayoutComponents/Footer';
 
 export default function MainLayout() {
   return (
     <>
     <Header />
     <main><Outlet /></main>
-    <footer>Footer</footer>
+    <Footer/>
     </>
   )
 }
