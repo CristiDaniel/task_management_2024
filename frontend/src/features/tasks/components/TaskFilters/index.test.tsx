cca00dedc0d47ab0226ddb2897444884dced4d8d
Cristi Daniel <danielcristi735@yahoo.com>
2024-12-15 23:04:39 +0200

    task(frontend): Added Unit Tests for TaskFilters

--- frontend/src/features/tasks/components/TaskFilters/index.test.tsx
+++ frontend/src/features/tasks/components/TaskFilters/index.test.tsx
@@ -1,41 +1,80 @@
 import { QueryClientProvider } from "@tanstack/react-query";
-import { beforeEach, vi } from "vitest";
+import { vi } from "vitest";
 import TaskFilters from ".";
 import { queryClient } from "../../../../main";
-import { render, screen } from "@testing-library/react";
-import { MemoryRouter, Route, Routes } from "react-router-dom";
+import { render, screen, waitFor } from "@testing-library/react";
+import { MemoryRouter as Router } from "react-router-dom";
+import * as TaskService from "../../services/taskService.ts";
 import userEvent from "@testing-library/user-event";
 
-beforeEach(() => {
-    vi.clearAllMocks();
-})
+vi.mock("../../services/taskService.ts");
 
-vi.mock("react-router-dom", async () => {
-    const actual = await vi.importActual("react-router-dom"); // Importul original
-    return {
-      ...actual, // Păstrează toate exporturile reale
-      useLocation: vi.fn(), // Mock doar pentru useLocation sau alte hook-uri
-    };
-  });
-  
-
+const MockedTaskService = vi.mocked(TaskService);
 const TaskFiltersContainer = () => (
+  <Router>
     <QueryClientProvider client={queryClient}>
-        <MemoryRouter>
-    <Routes>
-        <Route element={<TaskFilters />}/>
-    </Routes>
-      
-        </MemoryRouter>
+      <TaskFilters />
     </QueryClientProvider>
-  );
+  </Router>
+);
 
 describe("TaskFilters", () => {
-    
+  beforeEach(() => {
+    vi.resetAllMocks();
+  })
 
-    it('should render element', async() => {
-        render(<TaskFiltersContainer />)
+  
+  it("it should update select html element value", async () => {
+
+    render(<TaskFiltersContainer />);
+    await waitFor(() => {
+      
+      expect(screen.getByTestId("options", { exact: false })).toBeInTheDocument();
 
     })
+    
+  expect(screen.getByTestId("options")).toHaveValue("date_added_desc");
+
+  await userEvent.selectOptions(screen.getByTestId("options"), "date_added_asc");
+
+  expect(screen.getByTestId("options")).toHaveValue("date_added_asc");
+
 })
 
+it('should render checkbox filters based on enpoint data received', async() => {
+  MockedTaskService.countPriorityTasks.mockResolvedValueOnce(
+    {
+        "low": 3,
+        "medium": 2,
+        "high": 0
+    }
+)
+  render(<TaskFiltersContainer />)
+
+
+await waitFor(()=> {
+  expect(screen.getByText("Low", {exact: false})).toBeInTheDocument()  
+})
+})
+
+it('should not render checkbox filters when tasks has no different priorities', async () => {
+  MockedTaskService.countStatusTasks.mockResolvedValueOnce(
+    {
+      "pending": 4,
+      "in_progress": 0,
+      "completed": 0,
+      "on_hold": 0,
+      "cancelled": 0
+    }
+  )
+  render(<TaskFiltersContainer />)
+
+  await waitFor(() => {
+    expect(screen.queryByText("Filter by Status", {exact: false})).not.toBeInTheDocument()  
+    // expect(screen.queryByTestId("chart")).toBeInTheDocument()  
+    screen.debug()
+  })
+})
+
+
+})
